61c96d6420799cb5d7a71f7e8e71287a0d43b819
Midstackdev <midstackdev@gmail.com>
2019-09-19 20:06:26 +0100

    Sort by Asc and Desc

--- resources/js/components/DataTable.vue
+++ resources/js/components/DataTable.vue
@@ -9,13 +9,19 @@
                    <thead>
                        <tr>
                            <th v-for="column in response.displayable">
-                               {{column}}
+                               <span class="sortable" @click="sortBy(column)">{{column}}</span>
+
+                               <div 
+                               class="arrow" 
+                               v-if="sort.key === column"
+                               :class="{'arrow--asc' : sort.order === 'asc' , 'arrow--desc' : sort.order === 'desc'}"
+                               ></div>
                            </th>
                            <th>&nbsp;</th>
                        </tr>
                    </thead>
                    <tbody>
-                       <tr v-for="record in response.records">
+                       <tr v-for="record in filteredRecords">
                            <td v-for="columnValue, column in record">{{columnValue}}</td>
                            <td>
                                
@@ -38,18 +44,39 @@
                 response: {
                     table: '',
                     displayable: [],
-                    records: []
+                    records: [],
+                },
+
+                sort: {
+                    key: 'id',
+                    order: 'asc'
                 }
             }
         },
 
-        mounted() {
-            this.getRecords()
-        },
 
         computed: {
             tableName(){
-                return this.response.table
+                return String(this.response.table).toUpperCase()
+            },
+
+            filteredRecords() {
+                let data = this.response.records
+
+                if(this.sort.key) {
+                    data = _.orderBy(data, (i) => {
+
+                        let value = i[this.sort.key]
+                        
+                        if(!isNaN(parseFloat(value))) {
+                            return parseFloat(value)
+                        }
+
+                        return String(i[this.sort.key]).toLowerCase()
+                    }, this.sort.order)
+                }
+
+                return data
             }
         },
 
@@ -58,7 +85,43 @@
                 axios.get(`${this.endpoint}`).then(response => {
                     this.response = response.data.data
                 })
+            },
+
+            sortBy(column) {
+                this.sort.key = column
+                this.sort.order = this.sort.order === 'asc' ? 'desc' : 'asc'
             }
-        }
+        },
+
+        mounted() {
+            this.getRecords()
+        },
     }
 </script>
+
+<style lang="scss">
+    .sortable {
+        cursor: pointer;
+    }
+
+    .arrow {
+        display: inline-block;
+        vertical-align: middle;
+        width: 0px;
+        height: 0px;
+        margin-left: 5px;
+        opacity: .6;
+
+        &--asc {
+            border-left: 4px solid transparent;
+            border-right: 4px solid transparent;
+            border-bottom: 4px solid #222;
+        }
+
+        &--desc {
+            border-left: 4px solid transparent;
+            border-right: 4px solid transparent;
+            border-top: 4px solid #222;
+        }
+    }
+</style>
